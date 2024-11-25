f8becdaf0f745cae95c79981dffacf55a01849e8
Tiendvt <dovantientk@gmail.com>
2024-11-25 15:52:05 +0700

    demo1 spring security

---
+++ demo1/src/main/java/com/example/demo1/entity/Customer.java
@@ -0,0 +1,15 @@
+package com.example.demo1.entity;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class Customer {
+private String id;
+private String name;
+private String phoneNumber;
+private String email;
+}
